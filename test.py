bbfc78899ea52d54cb8ec531d1b5aa7638fcd06b
Abdelaziz-Merzoug <abdelaziz_merzoug@etu.univ-blida.dz>
2026-03-24 00:05:06 +0100

    More Exercises

--- test.py
+++ test.py
@@ -1,27 +1,54 @@
-secret = 7
+balance = 5000.0
+pin = "1234"
 
-name = input("What is your name?")
-print(f"Hello {name}! I'm thinking of a number between 1 and 10.")
+attemps = 0
 
-attempt = 1
-found = True
-while attempt <= 3:
-    try:
-        guess = int(input(f"Attempt {attempt}, enter your guess: "))
+while attemps < 3:
+    user_pin = input("Enter your pin: ")
+    if user_pin == pin:
+        print("Access granted")
+        break
+    else:
+        print("Access denied")
+        attemps += 1
 
-        if guess == secret:
-            print(f"🎉 Correct {name}! You got it on attempt {attempt}!")
-            found = False
-            break
-        elif guess > secret:
-            print("Too high, try again!")
+if attemps == 3:
+    print("Too many attempts. Access denied.")
+    exit()
+
+while True:
+    print("1. check balance")
+    print("2. deposit")
+    print("3. withdraw")
+    print("4. exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        print("Your balance is: ", balance)
+    elif choice == "2":
+        amount = float(input("Enter amount to deposit: "))
+        if amount <= 0:
+            print("Invalid amount. Please enter a positive number.")
+            continue
         else:
-            print("Too low, try again!")
+            balance += amount
+            print("Your new balance is: ", balance)
+    elif choice == "3":
+        amount = float(input("Enter amount to withdraw: "))
+        if amount <= 0:
+            print("Invalid amount. Please enter a positive number.")
+            continue
+        elif amount > balance:
+            print("Insufficient funds. Your balance is: ", balance)
+            continue
+        else:
+            balance -= amount
+            print("Your new balance is: ", balance)
+    elif choice == "4":
+        print("Thank you for using our ATM. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
 
-        attempt += 1
 
-    except ValueError:
-        print(f"Sorry {name}, you didn't enter a number!")
-
-if found:
-    print(f"Sorry {name}, the number was {secret}")
